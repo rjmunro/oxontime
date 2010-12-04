2bb72c82465fac1a1e3c8d20ce685a6a151fc5f7
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-12-04 15:43:47 +0000

    Added a GeoDjango PointField which is mapped from the oxontime x,y offsets.

--- oxontime/busses/models.py
+++ oxontime/busses/models.py
@@ -1,4 +1,10 @@
-from django.db import models
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db import models
+
+OFFSET_X, OFFSET_Y = (411026.07074929884, 131988.22506181369)
+OFFSET_X, OFFSET_Y = (415026.07074929884, 256088.22506181369)
+OFFSET_X, OFFSET_Y = (414721.07074929884, 256043.22506181369)
+SCALE_X, SCALE_Y = (2.2004998202088553, 2.1987468516915558)
 
 # Create your models here.
 class Region(models.Model):
@@ -16,3 +22,13 @@ class Bus(models.Model):
   updated = models.DateTimeField()
   x = models.IntegerField()
   y = models.IntegerField()
+
+  location = models.PointField(srid=27700)
+
+  def save(self, *args, **kwargs):
+      x = OFFSET_X + (self.region.x * 418 + self.x) * SCALE_X
+      y = OFFSET_Y - (self.region.y * 418 + self.y) * SCALE_Y
+
+      self.location = Point(x, y, srid=27700)
+
+      super(Bus, self).save(*args, **kwargs)
